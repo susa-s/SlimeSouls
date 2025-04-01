d8b768d4cce72aa0b89578050c67c76de9a39b78
susa-s <susashins@gmail.com>
2025-04-01 01:33:56 -1000

    Created Dragon hit box colliders

---
+++ Assets/AIDragonSFXManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AIDragonSFXManager : CharacterSFXManager
+{
+    [Header("Claw Swipes")]
+    public AudioClip[] clawSwipes;
+
+    [Header("Fire Breath")]
+    public AudioClip[] fireBreath;
+}
--- Assets/Project/Scripts/AIDragonCombatManager.cs
+++ Assets/Project/Scripts/AIDragonCombatManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class AIDragonCombatManager : AICharacterCombatManager
 {
+    AIBossCharacterManager aiBossManager;
+
     private ParticleSystem fireBreath;
 
     [Header("Damage Collider")]
@@ -11,8 +13,17 @@ public class AIDragonCombatManager : AICharacterCombatManager
 
     [Header("Damage")]
     [SerializeField] int baseDamage = 25;
+    [SerializeField] int fireDamage = 30;
     [SerializeField] float attack01DamageModifer = 1.0f;
     [SerializeField] float attack02DamageModifer = 1.4f;
+    [SerializeField] float attack03DamageModifer = 0.8f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        aiBossManager = GetComponent<AIBossCharacterManager>();
+    }
 
     private void Start()
     {
@@ -22,13 +33,12 @@ public class AIDragonCombatManager : AICharacterCombatManager
     public void SetAttack01Damage()
     {
         rightClawCollider.physicalDamage = baseDamage * attack01DamageModifer;
-        leftClawCollider.physicalDamage = baseDamage * attack01DamageModifer;
-        fireBreathCollider.physicalDamage = baseDamage * attack02DamageModifer;
+        leftClawCollider.physicalDamage = baseDamage * attack02DamageModifer;
     }
 
     public void SetAttack02Damage()
     {
-
+        fireBreathCollider.physicalDamage = fireDamage * attack02DamageModifer;
     }
 
     public void OpenRightClawCollider()
@@ -46,6 +56,7 @@ public class AIDragonCombatManager : AICharacterCombatManager
     {
         aiCharacter.characterSFXManager.PlayAttackGrunt();
         fireBreathCollider.EnableDamageCollider();
+        //aiBossManager.characterSFXManager.PlaySFX(WorldSFXManager.instance.ChooseRandomSFXFromArray(whooshes));
     }
 
     public void CloseRightClawCollider()
@@ -80,13 +91,29 @@ public class AIDragonCombatManager : AICharacterCombatManager
         if (aiCharacter.isPerformingAction)
             return;
         
-        if (viewableAngle >= 61 && viewableAngle <= 110)
+        if (viewableAngle >= 20 && viewableAngle <= 60)
+        {
+            aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("TurnR45", true);
+        }
+        else if (viewableAngle <= -20 && viewableAngle >= -60)
+        {
+            aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("TurnL45", true);
+        }
+        else if (viewableAngle >= 61 && viewableAngle <= 110)
         {
             aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("TurnR90", true);
         }
         else if (viewableAngle <= -61 && viewableAngle >= -110)
         {
             aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("TurnL90", true);
+        }
+        else if (viewableAngle >= 110 && viewableAngle <= 145)
+        {
+            aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("TurnR135", true);
+        }
+        else if (viewableAngle <= -110 && viewableAngle >= -145)
+        {
+            aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("TurnL135", true);
         }
         else if (viewableAngle >= 146 && viewableAngle <= 180)
         {
