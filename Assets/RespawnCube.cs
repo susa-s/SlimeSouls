92a335a84130999ed6408851f48582384b1e54f8
susa-s <susashins@gmail.com>
2025-04-09 16:53:25 -1000

    push

--- Assets/RespawnCube.cs
+++ Assets/RespawnCube.cs
@@ -4,6 +4,7 @@ public class RespawnCube : MonoBehaviour
 {
     [SerializeField] private float amplitude = 0.5f;
     [SerializeField] private float frequency = 1f;
+    [SerializeField] private float rotationSpeed = 1.5f;
 
     private Vector3 startPos;
 
@@ -16,5 +17,7 @@ public class RespawnCube : MonoBehaviour
     {
         float newY = startPos.y + Mathf.Sin(Time.time * frequency) * amplitude;
         transform.position = new Vector3(startPos.x, newY, startPos.z);
+        float newYRotation = Time.time * rotationSpeed;
+        transform.rotation *= Quaternion.Euler(0, newYRotation, 0);
     }
 }
